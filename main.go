bad2e5d0134e5a9d41dc48fc51223410732e2525
cakuang1 <cakuang1@berkeley.edu>
2023-12-11 11:30:58 -0800

    creating a webserver with load balncers

---
+++ main.go
@@ -0,0 +1,35 @@
+package loadbalancer
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":80", nil)
+}
+
+
+
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Received request from %s\n", r.RemoteAddr)
+
+	// Forward the request to a backend server
+	resp, err := http.Get("http://localhost:8080")
+	if err != nil {
+		http.Error(w, "Error forwarding request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	// Copy the backend server's response to the client
+	io.Copy(w, resp.Body)
+}
+
+
+
+
+
