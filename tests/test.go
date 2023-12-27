45d4e97702fb4d4b057d41e8c9f1913a2e1b18dc
cakuang1 <cakuang1@berkeley.edu>
2023-12-26 23:52:00 -0800

    tests

--- tests/test.go
+++ tests/test.go
@@ -1,8 +1,14 @@
 package tests
 
+
+
+
 import (
 	"testing"
 )
+
+
+
 /* One simple test case that determines if the key value pairs returned from single threaded version
 is the same as the multithreaded version*/
 
@@ -10,3 +16,21 @@ is the same as the multithreaded version*/
 
 
 
+func main() {
+	// Example usage
+	mr := SingleThreadedMR{
+		Tasks: []string{"file1.txt", "file2.txt", "file3.txt"},
+	}
+	// Process all files and aggregate the results
+	result := mr.Process()
+	fmt.Println("Aggregate Result:", result)
+}
+
+
+
+func main() {
+	// Example usage
+	mr := NewMultiThreadedMR([]string{"file1.txt", "file2.txt", "file3.txt"})
+	mr.Process()
+	fmt.Println("Processing completed.")
+}
